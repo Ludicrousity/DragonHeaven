477bd1be940df9dabe219b52fdb53b76f6e69b1c
Ludicrousity <nathangtcooper@gmail.com>
2017-05-14 14:00:47 +0100

    Create battle-engine.js
    Should be correct

---
+++ mods/eeveed/battle-engine.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const Tools = require('./tools');
+const PRNG = require('./prng');
+
+class BattlePokemon {
+                if (!ignoreImmunities && status.id && !(source && source.hasAbility('electrojection') && status.id in {'par': 1})) {
+			                  // the game currently never ignores immunities
+			                  if (!this.runStatusImmunity(status.id === 'par' : status.id)) {
+				                        this.battle.debug('immune to status');
+				                        if (sourceEffect && sourceEffect.status) this.battle.add('-immune', this, '[msg]');
+				                        return false;
+			                  }
+	            	}
+};
